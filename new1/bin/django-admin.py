a1d5891afaae872f8698d68c1ae0fcd0aec21799
16CO118 <16co118@example.com>
2018-10-20 11:42:29 +0530

    Bookmarking images

---
+++ new1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jayshinde/Desktop/ITA_REPOS/ITA_2/ITA_Connexion/new1/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
